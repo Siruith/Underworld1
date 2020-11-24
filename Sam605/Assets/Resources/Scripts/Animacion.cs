f141639e952c28850eb214c2ad8108fa6792fe8d
Sebastián <sebasbejar@gmail.com>
2020-11-23 20:27:12 -0500

    Cambios + Bosque

---
+++ Sam605/Assets/Resources/Scripts/Animacion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Animacion : MonoBehaviour
+{
+
+	public float speed = 3f;
+	public float turnSpeed = .1f;
+
+	public Animator anim;
+	public bool canMove;
+
+    void Update()
+    {
+		canMove = false;
+		if(Input.GetKeyDown(KeyCode.Space)){
+			canMove = true;
+		}
+		float strafe = Input.GetAxis("Horizontal");
+		float forward = Input.GetAxis("Vertical");
+		
+
+		transform.Translate( new Vector3(strafe, 0, forward) * Time.deltaTime * speed );
+
+		float turn = Input.GetAxis("Mouse X");
+
+		transform.rotation *= Quaternion.Slerp( Quaternion.identity, Quaternion.LookRotation(turn < 0 ? Vector3.left : Vector3.right), Mathf.Abs(turn) * Time.deltaTime * turnSpeed);
+
+		anim.SetFloat( "Right", strafe );
+		anim.SetFloat( "Forward", forward );
+		anim.SetBool( "CanMove", canMove );
+    }
+    
+}
---
+++ Sam605/Assets/Resources/Scripts/LeerTxt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LeerTxt : MonoBehaviour
+{
+    string pathFile;
+    public Vector3[] posiciones;
+    public GameObject myPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathFile = "Assets/Resources/Coords.txt";
+        creacionArboles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void creacionArboles(){
+        string[] fileline = File.ReadAllLines(pathFile);
+        posiciones = new Vector3[fileline.Length];
+
+        for(int i = 0; i < fileline.Length; i++){
+            string[] partes = fileline[i].Split("/"[0]);
+            float x = float.Parse(partes[0]);
+            float y = float.Parse(partes[1]);
+            float z = float.Parse(partes[2]);
+            posiciones[i] = new Vector3(x, y, z);
+        }
+        for(int i = 0; i < posiciones.Length; i++){
+            Debug.Log("Coordenadas leidas " + posiciones[i].ToString());
+            GameObject arbol = Instantiate(myPrefab, posiciones[i], Quaternion.identity);
+            
+        }
+        
+        
+
+    }
+}
